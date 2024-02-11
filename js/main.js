cf24d026b7ab6c74751f9b95bd2f3f7a37e3964b
Mohamed Amine Elmaaoui <63106502+mawi614@users.noreply.github.com>
2024-02-11 16:06:09 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*import * as THREE from 'three'
+
+//Scene
+const scene = new THREE.Scene()
+
+//Create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial(
+    {
+        color:"00ff83"
+    }
+    )
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
+//Adding a light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+//Adding a camera
+const camera = new THREE.PerspectiveCamera(45, 800/600, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(200,150)
+renderer.render(scene, camera)*/
