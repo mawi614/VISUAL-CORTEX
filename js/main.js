384561d42c9b960ece7b3f3f9b5c73f7e5a0d455
Mohamed Amine Elmaaoui <63106502+mawi614@users.noreply.github.com>
2024-02-18 17:21:55 +0100

    website 1.0 complete
    Added demo section and linked it, added partner section and linked it and added the contact form and the google app script to post the data to the google sheet.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
-/*import * as THREE from 'three'
+var sidemenu = document.getElementById("sidemenu");
 
-//Scene
-const scene = new THREE.Scene()
+function openmenu()
+{
+    sidemenu.style.right = "0";
+}
 
-//Create our sphere
-const geometry = new THREE.SphereGeometry(3, 64, 64)
-const material = new THREE.MeshStandardMaterial(
-    {
-        color:"00ff83"
-    }
-    )
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
-
-//Adding a light
-const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set(0, 10, 10)
-scene.add(light)
-
-//Adding a camera
-const camera = new THREE.PerspectiveCamera(45, 800/600, 0.1, 100)
-camera.position.z = 20
-scene.add(camera)
-
-//Renderer
-const canvas = document.querySelector(".webgl")
-const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(200,150)
-renderer.render(scene, camera)*/
+function closemenu()
+{
+    sidemenu.style.right = "-200px";
+}
